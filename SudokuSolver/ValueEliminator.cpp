40869a2ee0cdd3060a0e5faa39b226b2fc3f1388
Matthieu Dischamps <matthieu.dischamps@student.ecp.fr>
2015-02-27 21:54:38 +0100

    Created(ValueEliminator)

---
+++ SudokuSolver/ValueEliminator.cpp
@@ -0,0 +1,9 @@
+//
+//  ValueEliminator.cpp
+//  TestSudoku
+//
+//  Created by Matthieu on 27/02/15.
+//  Copyright (c) 2015 Maxime Bellec. All rights reserved.
+//
+
+#include "ValueEliminator.h"
---
+++ SudokuSolver/ValueEliminator.h
@@ -0,0 +1,14 @@
+//
+//  ValueEliminator.h
+//  TestSudoku
+//
+//  Created by Matthieu on 27/02/15.
+//  Copyright (c) 2015 Maxime Bellec. All rights reserved.
+//
+
+#ifndef __TestSudoku__ValueEliminator__
+#define __TestSudoku__ValueEliminator__
+
+#include <stdio.h>
+
+#endif /* defined(__TestSudoku__ValueEliminator__) */
